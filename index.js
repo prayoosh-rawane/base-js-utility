fe245e2ee46546504a0c7f9d2e50d7edda978869
Prayoosh Rawane <p.rawane@gmail.com>
2023-07-27 22:09:21 +0530

    npm init and added reverse string function

---
+++ index.js
@@ -0,0 +1,11 @@
+function reverseString (str = '') {
+    let revStr = '';
+    for(let i = str.length - 1; i>= 0; i--) {
+        revStr += str[i];
+    }
+    return revStr;
+}
+
+module.exports = {
+    reverseString
+}
